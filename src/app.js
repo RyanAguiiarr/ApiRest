93be491ddd94518ba03e4722587c047cb408a1e7
ryan <ryanzinlindo.4321@gmail.com>
2025-01-17 18:55:30 -0300

    adicionando cors

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import './database';
 import { resolve } from "path"
 
 import express from 'express';
+import cors from "cors"
+import helmet from 'helmet';
 
 
 import homeRoutes from './routes/homeRoutes';
@@ -19,6 +21,8 @@ import fotoRoutes from './routes/fotoRoutes';
     }
 
     middlewares(){
+      this.app.use(cors())
+      this.app.use(helmet())
       this.app.use(express.urlencoded({extended: true}))
       this.app.use(express.json())
       this.app.use('/images', express.static(resolve(__dirname, '..', 'uploads', 'images')));
