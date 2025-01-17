a49e6237c739c376973eef2eebbdd7c87498b762
ryan <ryanzinlindo.4321@gmail.com>
2025-01-17 18:56:46 -0300

    adicionando cors

--- dist/app.js
+++ dist/app.js
@@ -2,6 +2,8 @@
 var _path = require('path');
 
 var _express = require('express'); var _express2 = _interopRequireDefault(_express);
+var _cors = require('cors'); var _cors2 = _interopRequireDefault(_cors);
+var _helmet = require('helmet'); var _helmet2 = _interopRequireDefault(_helmet);
 
 
 var _homeRoutes = require('./routes/homeRoutes'); var _homeRoutes2 = _interopRequireDefault(_homeRoutes);
@@ -19,6 +21,8 @@ var _fotoRoutes = require('./routes/fotoRoutes'); var _fotoRoutes2 = _interopReq
     }
 
     middlewares(){
+      this.app.use(_cors2.default.call(void 0, ))
+      this.app.use(_helmet2.default.call(void 0, ))
       this.app.use(_express2.default.urlencoded({extended: true}))
       this.app.use(_express2.default.json())
       this.app.use('/images', _express2.default.static(_path.resolve.call(void 0, __dirname, '..', 'uploads', 'images')));
